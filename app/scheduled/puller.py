f3ecb92741a475a68b67ce20100c8a534a19fb56
Victor Nazario <victor.nazario@upr.edu>
2022-03-18 16:25:26 -0400

    Adds scheduled documentation, return types and tests

--- app/scheduled/puller.py
+++ app/scheduled/puller.py
@@ -3,9 +3,18 @@ import constant
 import schedule
 
 
-def make_pull(url: str):
-    feed = feedparser.parse(url)
+def make_pull(url: str) -> {}:
+    """
+    make_pull receives a URL linking to an RSS feed with relevant grant data to be acquired.
+    The obtained data is then filtered to only include grants relevant to Puerto Rico and
+    nonprofits. Returns a map with entries for the relevant columns, each containing a list of entries
+    :type url: str
+    :returns {}
+    """
+    feed = feedparser.parse(url, agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                       'Chrome/86.0.4240.183 Safari/537.36')
     data = {}
+
     for entry in feed.entries:
         if constant.NON_PROFITS_SEARCH_TERM in str(entry.content[0]['value']).lower() \
                 and constant.PR_SEARCH_TERM in str(entry.content[0]['value']).lower():
@@ -15,7 +24,8 @@ def make_pull(url: str):
             data['title'].append(entry.title)
             data['content'].append(entry.content)
             data['link'].append(entry.link)
-    print(len(data['link']))
+
+    return data
 
 
 def job():
