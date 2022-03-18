6f0910514f9741549fb86e6f69b5f97c4a01d9af
Victor Nazario <victor.nazario@upr.edu>
2022-03-18 16:52:55 -0400

    User agent and try/exc approach

--- app/scheduled/puller.py
+++ app/scheduled/puller.py
@@ -11,21 +11,26 @@ def make_pull(url: str) -> {}:
     :type url: str
     :returns {}
     """
-    feed = feedparser.parse(url, agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                       'Chrome/86.0.4240.183 Safari/537.36')
-    data = {}
+    try:
+        feed = feedparser.parse(url, agent='Feedfetcher-Google; (+http://www.google.com/feedfetcher.html; '
+                                           'feed-id=8639390370582375869)')
 
-    for entry in feed.entries:
-        if constant.NON_PROFITS_SEARCH_TERM in str(entry.content[0]['value']).lower() \
-                and constant.PR_SEARCH_TERM in str(entry.content[0]['value']).lower():
-            data.setdefault('title', [])
-            data.setdefault('content', [])
-            data.setdefault('link', [])
-            data['title'].append(entry.title)
-            data['content'].append(entry.content)
-            data['link'].append(entry.link)
+        data = {}
 
-    return data
+        for entry in feed.entries:
+            if constant.NON_PROFITS_SEARCH_TERM in str(entry.content[0]['value']).lower() \
+                    and constant.PR_SEARCH_TERM in str(entry.content[0]['value']).lower():
+                data.setdefault('title', [])
+                data.setdefault('content', [])
+                data.setdefault('link', [])
+                data['title'].append(entry.title)
+                data['content'].append(entry.content)
+                data['link'].append(entry.link)
+        return data
+
+    except ConnectionResetError:
+        print("connection err")  # Needs proper logging
+        return {}
 
 
 def job():
@@ -33,12 +38,11 @@ def job():
 
 
 def job2():
-    make_pull(constant.RSS_FEED_MOD_OP)
+    print(len(make_pull(constant.RSS_FEED_MOD_OP)))
 
 
 if __name__ == '__main__':
-    schedule.every().day.at("13:52").do(job)
-    schedule.every().day.at("13:53").do(job2)
+    schedule.every(10).seconds.do(job2)
 
     while True:
         schedule.run_pending()
