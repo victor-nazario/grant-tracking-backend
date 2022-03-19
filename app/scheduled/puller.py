a2ec5d0f6e4920167fab083d01294bbfec8ca2b1
Vincent <vincent.prado@upr.edu>
2022-03-18 21:22:43 -0400

    Added ETags to reduce bandwidth.

--- app/scheduled/puller.py
+++ app/scheduled/puller.py
@@ -3,7 +3,7 @@ import constant
 import schedule
 
 
-def make_pull(url: str) -> {}:
+def make_pull(url: str, feed_etag) -> {}:
     """
     make_pull receives a URL linking to an RSS feed with relevant grant data to be acquired.
     The obtained data is then filtered to only include grants relevant to Puerto Rico and
@@ -13,7 +13,10 @@ def make_pull(url: str) -> {}:
     """
     try:
         feed = feedparser.parse(url, agent='Feedfetcher-Google; (+http://www.google.com/feedfetcher.html; '
-                                           'feed-id=8639390370582375869)')
+                                           'feed-id=8639390370582375869)', etag=feed_etag)
+        if feed.status == 304:
+            print(feed.status)
+            return feed.feed
 
         data = {}
 
@@ -35,14 +38,18 @@ def make_pull(url: str) -> {}:
 
 def job():
     print('example')
+    print()
 
 
-def job2():
-    print(len(make_pull(constant.RSS_FEED_MOD_OP)))
+def job2(etag):
+    print(len(make_pull(constant.RSS_FEED_MOD_OP, feed_etag=etag)))
 
 
 if __name__ == '__main__':
-    schedule.every(10).seconds.do(job2)
+    initial_feed = feedparser.parse(constant.RSS_FEED_MOD_OP,
+                            agent='Feedfetcher-Google; (+http://www.google.com/feedfetcher.html; '
+                                  'feed-id=8639390370582375869)')
 
+    schedule.every(10).seconds.do(job2, etag=initial_feed.etag)
     while True:
         schedule.run_pending()
