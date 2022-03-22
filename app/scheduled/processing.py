8bcf880c1069c63e86e79719c7173495f8ef1178
Victor Nazario <victor.nazario@upr.edu>
2022-03-21 21:46:45 -0400

    Improvements to handle dates with application instructions

--- app/scheduled/processing.py
+++ app/scheduled/processing.py
@@ -2,5 +2,6 @@ from datetime import datetime
 
 
 def obtain_close_date(entry: str) -> datetime:
-    date_string = entry.partition("Close Date:</td><td>")[2].partition("</td></tr><tr><td>")[0].strip()
+    unprocessed = entry.partition("Close Date:</td><td>")[2].partition("</td></tr><tr><td>")[0]
+    date_string = unprocessed[0:13].strip()
     return datetime.strptime(date_string, '%b %d, %Y')
--- app/scheduled/processing_test.py
+++ app/scheduled/processing_test.py
@@ -48,9 +48,82 @@ test_description = "<![CDATA[ <TABLE BORDER=0 WIDTH='100%'><TR><TD><table><tr><t
                    "https://www.nsf.gov/pubs/2019/nsf19607/nsf19607.htm.</td></tr><tr><td>Version:</td><td>7</td></tr" \
                    "><tr><td>Modification Comments:</td><td>.</td></tr></table></TD></TR></TABLE> ]]> "
 
+test_description_contains_extra_wording = "<![CDATA[ <TABLE BORDER=0 WIDTH='100%'><TR><TD><table><tr><td>Funding " \
+                                          "Opportunity ID: </td><td>323740</td></tr><tr><td>Opportunity Number: " \
+                                          "</td><td>2020-NIST-MEP-MDAP-01</td></tr><tr><td>Opportunity " \
+                                          "Title:</td><td>NIST MEP Disaster Assessment " \
+                                          "Program</td></tr><tr><td>Opportunity " \
+                                          "Category:</td><td>Discretionary</td></tr><tr><td>Opportunity Category " \
+                                          "Explanation:</td><td></td></tr><tr><td valign='top'>Funding Instrument " \
+                                          "Type: </td><td>Cooperative Agreement</td></tr><tr><td " \
+                                          "valign='top'>Category of Funding Activity: </td><td>Other (see text field " \
+                                          "entitled Explanation of Other Category of Funding Activity for " \
+                                          "clarification)</td></tr><tr><td valign='top'>Category Explanation: " \
+                                          "</td><td>Manufacturing Extension Program</td></tr><tr><td " \
+                                          "valign='top'>CFDA Number(s): </td><td>11.611</td></tr><tr><td " \
+                                          "valign='top'>Eligible Applicants:</td><td>Others (see text field entitled " \
+                                          "Additional Information on Eligibility for clarification)</td></tr><tr><td " \
+                                          "valign='top'>Additional Information on Eligibility:</td><td>Eligible " \
+                                          "applicants for this funding opportunity are recipients of current MEP " \
+                                          "Center cooperative agreements. A MEP Center recipient may work " \
+                                          "individually or may include proposed subawards to other recipients of MEP " \
+                                          "Center cooperative agreements and/or proposed contracts with other " \
+                                          "organizations as part of the applicant&#8217;s proposal, effectively " \
+                                          "forming a team or consortium. A MEP Center may only submit one funding " \
+                                          "application for each FEMA Disaster Declaration and, where an application " \
+                                          "is submitted with respect to a particular FEMA Disaster Declaration, " \
+                                          "such MEP Center may not be included as a subawardee or as a project " \
+                                          "participant in any other applications pertaining to the same FEMA Disaster " \
+                                          "Declaration. Moreover, a MEP Center that does not submit a funding " \
+                                          "application for a particular FEMA Disaster Declaration may only be " \
+                                          "included as a subawardee or as a project participant in one funding " \
+                                          "application for each FEMA Disaster Declaration.</td></tr><tr><td " \
+                                          "valign='top'>Agency Code:</td><td>DOC-NIST</td></tr><tr><td " \
+                                          "valign='top'>Agency Name:</td><td>Department of Commerce<br>National " \
+                                          "Institute of Standards and Technology</td></tr><tr><td>Posted " \
+                                          "Date:</td><td>Jan 17, 2020</td></tr><tr><td>Close Date:</td><td>Apr 01, " \
+                                          "2023 Applications for funding pursuant to this NOFO must be received by " \
+                                          "NIST no later than 60 calendar days following the date that a Major or an " \
+                                          "Emergency Disaster Declaration is declared by the Federal Emergency " \
+                                          "Management Agency (FEMA Disaster Declaration) for all or a portion of an " \
+                                          "impacted State or of Puerto Rico. NIST expects to complete its review, " \
+                                          "selection of successful applicants, and award processing within 90 " \
+                                          "calendar days of application receipt. This program is open, " \
+                                          "and applications will be accepted by NIST on a rolling basis based on the " \
+                                          "date of the subject FEMA Disaster Declaration and subject to the " \
+                                          "publication of a superseding NOFO. See Section IV.4. in the Full " \
+                                          "Announcement Text of this NOFO.</td></tr><tr><td>Last Updated " \
+                                          "Date:</td><td>Feb 24, 2022</td></tr><tr><td>Award " \
+                                          "Ceiling:</td><td>$0</td></tr><tr><td>Award " \
+                                          "Floor:</td><td>$0</td></tr><tr><td>Estimated Total Program " \
+                                          "Funding:</td><td></td></tr><tr><td>Expected Number of " \
+                                          "Awards:</td><td></td></tr><tr><td>Description:</td><td>NIST invites " \
+                                          "applications from current recipients of Manufacturing Extension " \
+                                          "Partnership Center cooperative agreements (MEP Centers) to perform " \
+                                          "assessments of small- and medium-sized manufacturers (SMMs) in areas " \
+                                          "subject to a FEMA Disaster Declaration. These assessments should be " \
+                                          "designed to identify the impact, if any, to the operations of the SMMs as " \
+                                          "result of the subject disaster. MEP Centers receiving funding pursuant to " \
+                                          "this program must also assist impacted SMMs in identifying and accessing " \
+                                          "Federal, State and local resources to aid in business recovery efforts " \
+                                          "and, as appropriate, in the development of a risk mitigation plan for " \
+                                          "future disasters. Award recipients will further be required to share the " \
+                                          "results of their project, including disaster preparedness lessons learned " \
+                                          "and SMMs best practices, with other SMMs, NIST and the MEP National " \
+                                          "NetworkTM in order to help the SMM community with future disaster " \
+                                          "resilience planning efforts. See Section I. in the Full Announcement Text " \
+                                          "of this " \
+                                          "NOFO.</td></tr><tr><td>Version:</td><td>4</td></tr><tr><td>Modification " \
+                                          "Comments:</td><td>Update to close " \
+                                          "date.</td></tr></table></TD></TR></TABLE> ]]> "
+
+test_values = [test_description, test_description_contains_extra_wording]
+
 
 def test_obtain_close_date():
-    assert type(obtain_close_date(test_description)) is datetime, "Expected returned value to be of type datetime"
+    for value in test_values:
+        assert type(obtain_close_date(value)) is datetime, "Expected returned value to " \
+                                                                                         "be of type datetime "
 
 
 if __name__ == '__main__':
