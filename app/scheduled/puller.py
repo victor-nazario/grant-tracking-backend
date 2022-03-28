ca0fd39da20360254e6ea76094671a87d4df6cc1
Victor Nazario <victor.nazario@upr.edu>
2022-03-28 15:57:01 -0400

    Allows for an etag to be pased to the make pull call for re-usability purposes.

--- app/scheduled/puller.py
+++ app/scheduled/puller.py
@@ -4,38 +4,38 @@ import feedparser
 
 from app.scheduled import constant
 
+etag_dict = {
+    'new_op_etag': '',
+    'mod_op_etag': ''
+}
 
-def make_pull(url: str) -> []:
+
+def make_pull(url: str, previous_etag: str) -> []:
     """
     make_pull receives a URL linking to an RSS feed with relevant grant data to be acquired.
     The obtained data is then filtered to only include grants relevant to Puerto Rico and
     nonprofits. Returns a map with entries for the relevant columns, each containing a list of entries
-    :type url: str
+    :param previous_etag: Str representing the prev. known etag of the RSS. Used to identify if feed is already parsed.
+    :param url: The url where the feed to pull is available
     :returns {}
     """
     try:
-        # load your data back to memory when you need it
-        with open('mypickle.pk', 'rb') as etag_file:
-            previous_etag = pickle.load(etag_file)
-
         feed = feedparser.parse(url, etag=previous_etag)
+        if url is constant.RSS_FEED_NEW_OP:
+            etag_dict['new_op_etag'] = feed.etag
+        else:
+            etag_dict['mod_op_etag'] = feed.etag
+
         if feed.status == 304:
             print(feed.status)
             print(feed.etag)
             return feed.feed
-
-        # open a pickle file
-        file_name = 'mypickle.pk'
-        with open(file_name, 'wb') as etag_file:
-            # dump your data into the file
-            pickle.dump(feed.etag, etag_file)
 
         entry_list = []
 
         for entry in feed.entries:
             if constant.NON_PROFITS_SEARCH_TERM in str(entry.content[0]['value']).lower() \
                     and constant.PR_SEARCH_TERM in str(entry.content[0]['value']).lower():
-
                 entry_list.append({
                     'title': entry.title,
                     'content': entry.content,
@@ -47,3 +47,18 @@ def make_pull(url: str) -> []:
     except ConnectionResetError:
         print("connection err")  # Needs proper logging
         return {}
+
+
+if __name__ == '__main__':
+    # load your data back to memory when you need it
+    with open('mypickle.pk', 'rb') as etag_file:
+        etag_dict_loaded = pickle.load(etag_file)
+        print(etag_dict_loaded)
+        print(len(make_pull(constant.RSS_FEED_NEW_OP, etag_dict_loaded['new_op_etag'])))
+        print(len(make_pull(constant.RSS_FEED_MOD_OP, etag_dict_loaded['mod_op_etag'])))
+
+    # open a pickle file
+    file_name = 'mypickle.pk'
+    with open(file_name, 'wb') as etag_file:
+        # dump your data into the file
+        pickle.dump(etag_dict, etag_file)
