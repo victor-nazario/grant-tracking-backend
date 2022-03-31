51500760d3277e2e76f31f3859d7ccc1878379a1
Vincent <vincent.prado@upr.edu>
2022-03-31 13:28:45 -0400

    Change to create sessions using get_session function.

--- app/scheduled/deletion.py
+++ app/scheduled/deletion.py
@@ -1,8 +1,8 @@
-from app.scheduled.models import db_session, GrantEntry
+from app.scheduled.models import GrantEntry, get_session
 from datetime import datetime
 
 
 def delete_grant():
-    session = db_session()
+    session = get_session()
     session.query(GrantEntry).filter(datetime.utcnow() > GrantEntry.close_date).delete()
     session.commit()
