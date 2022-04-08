04612dc201f0d5474cec6cd1c2e92cded3b802d5
Vincent <vincent.prado@upr.edu>
2022-04-08 13:20:55 -0400

    Added APISpec.

--- app/api/grants.py
+++ app/api/grants.py
@@ -2,9 +2,24 @@ from flask import Blueprint, jsonify
 from app.scheduled.layers import models
 from app.session_generator.create_session import get_session
 from app.scheduled.layers.models import GrantEntrySchema
+from apispec import APISpec
+from apispec_webframeworks.flask import FlaskPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
 
 grants_bp = Blueprint('api', __name__)
 grant_schema = GrantEntrySchema()
+
+spec = APISpec(
+    title='Grant-Tracking-Platform-Swagger-Documentation',
+    version='1.0.0',
+    openapi_version='3.0.2',
+    plugins=[FlaskPlugin(), MarshmallowPlugin()]
+)
+
+
+@grants_bp.route('/swagger.json')
+def create_swagger_spec():
+    return jsonify(spec.to_dict())
 
 
 @grants_bp.route('/grants', methods=['GET'])
@@ -18,4 +33,6 @@ def available_grants():
         grant_list.append(grant_schema.dump(grant))
 
     return jsonify(grant_list)
+
+
 
