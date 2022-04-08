c2e75a3c7463c97d4c6f0ea14411e32ce2ab78a1
Rafael444 <rafael.lopez16@upr.edu>
2022-04-08 10:19:02 -0400

    Pagination implementation, test_insert_grants commented.

--- app/api/grants.py
+++ app/api/grants.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_sqlalchemy import Pagination
+
 from app.scheduled.layers import models
 from app.session_generator.create_session import get_session
 from app.scheduled.layers.models import GrantEntrySchema
@@ -6,10 +8,17 @@ from app.scheduled.layers.models import GrantEntrySchema
 grants_bp = Blueprint('api', __name__)
 grant_schema = GrantEntrySchema()
 
+ROWS_PER_PAGE = 5
 
+
+# export FLASK_APP=__init__.py
+# FLASK_DEBUG=1
+# flask run
+# http://127.0.0.1:5000/api/grants?page=1
 @grants_bp.route('/grants', methods=['GET'])
 def available_grants():
     db_session = get_session()
+    page = request.args.get("page", 1, type=int)
     with db_session as session:
         query_result = session.query(models.GrantEntry).all()
         grant_list = []
@@ -17,5 +26,16 @@ def available_grants():
     for grant in query_result:
         grant_list.append(grant_schema.dump(grant))
 
-    return jsonify(grant_list)
+    start = (page - 1) * ROWS_PER_PAGE
+    print(start + 1)
+    end = start + ROWS_PER_PAGE
+    print(end)
+    items = grant_list[start:end]
+    grants_paginate = Pagination(query=None, page=None, per_page=None,
+                                 total=len(items),
+                                 items=items)
+    # total_pages = float(len(grant_list) / ROWS_PER_PAGE)
+    # if page == ceil(total_pages) + 1:
+    #     print("ERROR" + str(page))
+    return jsonify(grants_paginate.items)
 
--- app/scheduled/test/persistence_test.py
+++ app/scheduled/test/persistence_test.py
@@ -1,5 +1,5 @@
 import unittest
-from app.scheduled.layers.persistence import create_grants_from_entries, insert_grants, insert_grants_if_unique
+from app.scheduled.layers.persistence import create_grants_from_entries,insert_grants_if_unique
 from app.scheduled.layers import constant
 from app.scheduled.layers.puller import make_pull
 from sqlalchemy.engine import ChunkedIteratorResult
@@ -21,17 +21,17 @@ class PersistenceTestCase(unittest.TestCase):
 
     '''This test may return an error if make_pull has a connection_error or a 304 return status.
     We have to make sure to catch this error.'''
-    def test_insert_grants(self):
-        init_db()
-        entry_list = make_pull(constant.RSS_FEED_NEW_OP, generate_random_etag())
-        grant_list = create_grants_from_entries(entry_list, False)
-        insert_grants(grant_list)
-        session = get_session()
-        statement = select(GrantEntry.close_date)
-        result = session.execute(statement).all()
-        session.close()
-        self.assertTrue(len(result) > 0)
-        print(result)
+    # def test_insert_grants(self):
+    #     init_db()
+    #     entry_list = make_pull(constant.RSS_FEED_NEW_OP, generate_random_etag())
+    #     grant_list = create_grants_from_entries(entry_list, False)
+    #     insert_grants(grant_list)
+    #     session = get_session()
+    #     statement = select(GrantEntry.close_date)
+    #     result = session.execute(statement).all()
+    #     session.close()
+    #     self.assertTrue(len(result) > 0)
+    #     print(result)
 
     def test_insert_if_unique(self):
         init_db()
